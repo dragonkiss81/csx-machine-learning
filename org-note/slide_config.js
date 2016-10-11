065872cbd174c8b08581b4772fc796fa87cc5172
rcwangtw <rcwang.tw@gmail.com>
2016-10-11 10:01:34 +0800

    測試網頁功能

---
+++ org-note/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: '特徵選取 Feature Selection', 
+     subtitle: '少年圖靈計畫', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/ricky.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'rcwangtw', 
+     company: '', 
+     gplus: 'https://plus.google.com/rcwangtw', 
+     twitter: 'rcwangtw', 
+     www: 'http://rcwangtw.github.io/', 
+     github: 'http://github.com/rcwangtw', 
+   }]
+};
